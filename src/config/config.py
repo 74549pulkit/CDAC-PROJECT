59bb0d25f4b52f4e44ff2f3728756e1f3efbe9eb
Pulkit kushwaha <125993383+74549pulkit@users.noreply.github.com>
2024-03-01 16:44:29 +0530

    Add files via upload

--- src/config/config.py
+++ src/config/config.py
@@ -1,6 +1,18 @@
 config = {
     "openai":{
-        "api_key":"OPENAI_KEY"
+        "api_key":"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"
+    },
+    "kafka": {
+        "sasl.username": "4FNUGMIMWURCADWI",
+        "sasl.password": "c79BcbOH6FLqgG2wKBSqKDYaO1fuS2RG5FfBE8NVBpGqOmVHfPwdie63derP4kRo",
+        "bootstrap.servers": "pkc-4r087.us-west2.gcp.confluent.cloud:9092",
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms': 'PLAIN',
+        'session.timeout.ms': 45000
+    },
+    "schema_registry": {
+        "url": "https://psrc-4r3n1.us-central1.gcp.confluent.cloud",
+        "basic.auth.user.info": "2RCRTTORPOXGXRJD:ERAWBK2VotvpwVsgR62nsnJnD/J38tZfLnCPdSqtV65OBr7f+IlC/uL3Iajc1pOQ",  # username:password
     }
 
 }
